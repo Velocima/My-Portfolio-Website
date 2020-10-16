030e8e20c0435b46fc6793495ebe7df086f3fb51
Velo <hartley.max@outlook.com>
2020-10-16 01:39:21 +0100

    silly custom cursor added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/app.css';
 
 export default function App() {
-  return (
-    <div className="app">
-      <h1>Hello World</h1>
-    </div>
-  )
+	const [ cursorX, setCursorX ] = useState(0);
+	const [ cursorY, setCursorY ] = useState(0);
+
+	const updateMousePosition = ({clientX, clientY}) => {
+		setCursorX(clientX);
+		setCursorY(clientY);
+	}
+	useEffect(()=> {
+		document.addEventListener('mousemove', updateMousePosition)
+		return () => {
+			document.removeEventListener('mousemove', updateMousePosition)
+		}
+	},[cursorX, cursorY]);
+	
+	return (
+		<div className="app">
+			<div className='cursor' style={{transform: `translateY(${cursorY}px) translateX(${cursorX}px)`}}></div>
+			<h1>Max Hartley</h1>
+		</div>
+	)
 }
 
