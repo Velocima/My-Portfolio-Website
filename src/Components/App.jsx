d3a30fb47f420c0e4aa21abdb4e00967fe1c24f1
Velo <hartley.max@outlook.com>
2020-10-19 14:54:02 +0100

    first version of dark mode added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import '../css/app.css';
 
 export default function App() {
-	const [ cursorX, setCursorX ] = useState(0);
-	const [ cursorY, setCursorY ] = useState(0);
+	const [ cursorX, setCursorX ] = useState(-10);
+	const [ cursorY, setCursorY ] = useState(-10);
+	const [ colorMode, setColorMode ] = useState(true);
 
 	const updateMousePosition = ({clientX, clientY}) => {
 		setCursorX(clientX);
@@ -16,10 +17,26 @@ export default function App() {
 		}
 	},[cursorX, cursorY]);
 	
+	const cursorStyle = {
+		transform: `translateY(${cursorY-10}px) translateX(${cursorX-10}px)`,
+		color: '#f9f7f7',
+	}
+
+	const handleColorModeClick = ({ target }) => {
+		setColorMode(prev => !prev);
+	}
+
+	const darkFilterStyle = {
+        height: colorMode ? "0" : "250vw",
+		width: colorMode ? "0" : "250vw",
+	}
+	
 	return (
 		<div className="app">
-			<div className='cursor' style={{transform: `translateY(${cursorY}px) translateX(${cursorX}px)`}}></div>
-			<h1>Max Hartley</h1>
+			<div className='cursor' style={cursorStyle}></div>
+			<div className='dark-filter' style={darkFilterStyle}></div>
+			<div className="color-mode" onClick={handleColorModeClick}></div>
+			<h1>MAX HARTLEY</h1>
 		</div>
 	)
 }
