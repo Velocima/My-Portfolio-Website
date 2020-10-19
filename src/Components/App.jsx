f13917b7c9e7508e5bce4ad78544e413d256ad9b
Velo <hartley.max@outlook.com>
2020-10-19 15:09:00 +0100

    cursor seperated to component from app

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Cursor } from './Cursor';
 import '../css/app.css';
 
 export default function App() {
-	const [ cursorX, setCursorX ] = useState(-10);
-	const [ cursorY, setCursorY ] = useState(-10);
 	const [ colorMode, setColorMode ] = useState(true);
-
-	const updateMousePosition = ({clientX, clientY}) => {
-		setCursorX(clientX);
-		setCursorY(clientY);
-	}
-	useEffect(()=> {
-		document.addEventListener('mousemove', updateMousePosition)
-		return () => {
-			document.removeEventListener('mousemove', updateMousePosition)
-		}
-	},[cursorX, cursorY]);
-	
-	const cursorStyle = {
-		transform: `translateY(${cursorY-10}px) translateX(${cursorX-10}px)`,
-		color: '#f9f7f7',
-	}
 
 	const handleColorModeClick = ({ target }) => {
 		setColorMode(prev => !prev);
@@ -33,7 +16,7 @@ export default function App() {
 	
 	return (
 		<div className="app">
-			<div className='cursor' style={cursorStyle}></div>
+			<Cursor />
 			<div className='dark-filter' style={darkFilterStyle}></div>
 			<div className="color-mode" onClick={handleColorModeClick}></div>
 			<h1>MAX HARTLEY</h1>
---
+++ src/Components/Cursor.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+export const Cursor = () => {
+	const [ cursorX, setCursorX ] = useState(-10);
+    const [ cursorY, setCursorY ] = useState(-10);
+    
+	const updateMousePosition = ({clientX, clientY}) => {
+		setCursorX(clientX);
+		setCursorY(clientY);
+    }
+    
+	useEffect(()=> {
+		document.addEventListener('mousemove', updateMousePosition)
+		return () => {
+			document.removeEventListener('mousemove', updateMousePosition)
+		}
+	},[cursorX, cursorY]);
+
+    const cursorStyle = {
+		transform: `translateY(${cursorY-10}px) translateX(${cursorX-10}px)`,
+		color: '#f9f7f7',
+    }
+    
+    return <div className='cursor' style={cursorStyle}></div>
+}
