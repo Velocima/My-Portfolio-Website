7dcfd35880a155300a34f715003477cebbcb68d4
Velo <hartley.max@outlook.com>
2020-10-21 22:35:15 +0100

    bouncyword code cleaned

--- src/Components/BouncyWord.jsx
+++ src/Components/BouncyWord.jsx
@@ -3,38 +3,38 @@ import React, { useState } from 'react';
 export const BouncyWord = (props) => {
     const { isLinkHovered, isLinkUnhovered, word } = props;
     const [ animated, setAnimated ] = useState(false);
-    const handleMouseOver = ({ target }) => {
+    const handleMouseOver = () => {
         isLinkHovered();
         setAnimated(true)
     }
     const handleMouseLeave = () => {
         isLinkUnhovered();
     }
-    const handleAnimationEnd = ({ target }) => {
+    const handleAnimationEnd = () => {
         setAnimated(false)
     }
-return (
-    <p
-        id={word}
-        className={animated ? "animated" : ""}
-        onMouseEnter={handleMouseOver} 
-        onMouseLeave={handleMouseLeave} 
-    >{
-        word
-            .split('')
-            .map((letter, spanIndex) => {
-                return (
-                    <span 
-                        key={spanIndex} 
-                        onAnimationEnd={
-                            spanIndex === word.length -1 ? 
-                            handleAnimationEnd : 
-                            function() {}
-                        }
-                    >
-                        {letter}
-                    </span>)
-            })
-        }
-    </p>)
+    return (
+        <p
+            id={word}
+            className={animated ? "animated" : ""}
+            onMouseEnter={handleMouseOver} 
+            onMouseLeave={handleMouseLeave} 
+        >{
+            word
+                .split('')
+                .map((letter, spanIndex) => {
+                    return (
+                        <span 
+                            key={spanIndex} 
+                            onAnimationEnd={
+                                spanIndex === word.length -1 ? 
+                                handleAnimationEnd : 
+                                function() {}
+                            }
+                        >
+                            {letter}
+                        </span>)
+                })
+            }
+        </p>)
 }
