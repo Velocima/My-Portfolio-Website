73792d500cecf5d69ac854620ec5ad17afe7dbc7
Velo <hartley.max@outlook.com>
2020-10-21 15:27:25 +0100

    wave animation added on hover to nav links

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -7,20 +7,52 @@ import '../css/nav.css'
 
 export const Nav = (props) => {
     const pages = ['About', 'Projects', 'Contact'];
-    const handleMouseOver = () => {
+    const [ animated, setAnimated ] = useState([]);
+    const handleMouseOver = ({ target }) => {
         props.isLinkHovered();
+        setAnimated(prev => [...prev, target.parentNode.id])
     }
     const handleMouseLeave = () => {
         props.isLinkUnhovered();
+    }
+    const handleAnimationEnd = ({ target }) => {
+        setAnimated(prev => prev.filter(listId => listId !== target.parentNode.id))
     }
 
     return (<Router>
         <nav className="nav">
             <ul>
-                {pages.map(link => {
-                    return <li onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave}><Link to={() => `/${link.toLowerCase()}`}>{link}</Link></li>
+                {pages.map((link, pagesIndex) => {
+                    return (<Link key={pagesIndex} to={() => `/${link.toLowerCase()}`}>
+                            <li 
+                                id={link}
+                                className={animated.includes(link) ? "animated" : ""}
+                                onMouseEnter={handleMouseOver} 
+                                onMouseLeave={handleMouseLeave} 
+                                >
+                                {
+                                link
+                                    .split('')
+                                    .map((letter, spanIndex) => {
+                                    return (<span 
+                                                key={spanIndex} 
+                                                onAnimationEnd={spanIndex === link.length -1 ? handleAnimationEnd : function() {}}
+                                                >
+                                                {letter}
+                                            </span>)
+                                })
+                                }
+                            </li>
+                        </Link>)
                 })}
             </ul>
         </nav>
     </Router>)
 }
+
+
+// return (<li onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave}>
+//     <Link to={() => `/${link.toLowerCase()}`}>
+//         {link.split('').map(letter => <span>{letter}</span>)}
+//     </Link>
+// </li>)
