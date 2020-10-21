0a26a03d2d353d9965418a9710e2e0f9187339f1
Velo <hartley.max@outlook.com>
2020-10-21 17:09:13 +0100

    darkmode filter saparated to component. darkmod scss separted to on file from app.scss

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
 import { Cursor } from './Cursor';
+import { DarkMode } from './DarkMode';
 import { Nav } from './Nav';
 import '../css/app.css';
 
 export default function App() {
-	// Dark mode filter state and functions
-	const [ colorMode, setColorMode ] = useState(true);
-
-	const handleColorModeClick = ({ target }) => {
-		setColorMode(prev => !prev);
-	}
-
-	const darkFilterStyle = {
-        height: colorMode ? "0" : "250vw",
-		width: colorMode ? "0" : "250vw",
-	}
-	
 	// Cursor hover state and functions
 	const [ linkHovered, setLinkHovered ] = useState(false);
 
@@ -29,8 +18,7 @@ export default function App() {
 	return (
 		<div className="app">
 			<Cursor isLinkHovered={linkHovered}/>
-			<div className='dark-filter' style={darkFilterStyle}></div>
-			<div className="color-mode" onClick={handleColorModeClick}></div>
+			<DarkMode />
 			<h1>MAX HARTLEY</h1>
 			<Nav isLinkHovered={handleMouseEnterLink} isLinkUnhovered={handleMouseLeaveLink}/>
 		</div>
---
+++ src/Components/DarkMode.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import '../css/darkmode.css';
+
+export const DarkMode = () => {
+	const [ colorMode, setColorMode ] = useState(true);
+
+	const handleColorModeClick = ({ target }) => {
+		setColorMode(prev => !prev);
+	}
+
+	const darkFilterStyle = {
+        height: colorMode ? "0" : "250vw",
+		width: colorMode ? "0" : "250vw",
+	}
+    
+    return (<div>
+                <div className='dark-filter' style={darkFilterStyle}></div>
+                <div className="color-mode" onClick={handleColorModeClick}></div>
+            </div>)
+}
